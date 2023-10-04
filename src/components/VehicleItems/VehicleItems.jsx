3a19eab50451e791e733898f776b2a6e779cb5d4
letmechek <abdullahi.ali1017@gmail.com>
2023-10-04 05:18:18 -0400

    fixes product card ui

--- src/components/VehicleItems/VehicleItems.jsx
+++ src/components/VehicleItems/VehicleItems.jsx
@@ -1,25 +1,63 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Button,
+} from "@material-tailwind/react";
 
 export default function VehicleItems({ vehicleItem }) {
   return (
-    <div className="bg-white p-4 m-4 shadow-xl rounded-lg hover:shadow-2xl flex flex-col items-center justify-between ">
+    <Card className="bg-white p-4 m-4 shadow-xl rounded-lg hover:shadow-2xl flex flex-col items-center justify-between ">
       <Link
         to={`/${vehicleItem.name}/${vehicleItem._id}`}
         className=""
       >
+      <CardHeader shadow={false} floated={false} className="h-96">
+
         <img
           src={vehicleItem.image[0]}
           alt={vehicleItem.name}
-          className="h-40 w-full object-cover rounded-lg hover:scale-105 transition-transform duration-100 ease-in-out"
+          className="h-full w-full object-cover"
         />
-        <div className="h-18 text-center">
+        </CardHeader>
+        <CardBody>
+        <div className="mb-2 flex items-center justify-between">
           <h2 className="text-lg mt-2 overflow-hidden">{vehicleItem.name}</h2>
         </div>
         <div className="mt-4">
           <p className="text-green-700 font-bold">${vehicleItem.price}</p>
         </div>
+        </CardBody>
       </Link>
-    </div>
+    </Card>
   );
 }
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+
+// export default function VehicleItems({ vehicleItem }) {
+//   return (
+//     <div className="bg-white p-4 m-4 shadow-xl rounded-lg hover:shadow-2xl flex flex-col items-center justify-between ">
+//       <Link
+//         to={`/${vehicleItem.name}/${vehicleItem._id}`}
+//         className=""
+//       >
+//         <img
+//           src={vehicleItem.image[0]}
+//           alt={vehicleItem.name}
+//           className="h-40 w-full object-cover rounded-lg hover:scale-105 transition-transform duration-100 ease-in-out"
+//         />
+//         <div className="h-18 text-center">
+//           <h2 className="text-lg mt-2 overflow-hidden">{vehicleItem.name}</h2>
+//         </div>
+//         <div className="mt-4">
+//           <p className="text-green-700 font-bold">${vehicleItem.price}</p>
+//         </div>
+//       </Link>
+//     </div>
+//   );
+// }
--- src/pages/Vehicle/Vehicle.jsx
+++ src/pages/Vehicle/Vehicle.jsx
@@ -18,7 +18,7 @@ export default function Vehicle() {
     }, [])
   return (
     <>
-  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
     {vehicleItems.map((vehicle, index) => (
       <VehicleItems key={vehicle._id} vehicleItem={vehicle} />
       ))}
