133dc8f8f61178c244f4e50a4bc79163692ed2e1
letmechek <abdullahi.ali1017@gmail.com>
2023-10-05 16:05:51 -0400

    fixes cart icon

--- src/components/CartIcon/CartIcon.jsx
+++ src/components/CartIcon/CartIcon.jsx
@@ -3,19 +3,19 @@ import { Link } from 'react-router-dom';
 import { ShoppingCartIcon } from '@heroicons/react/24/solid'; // Import the shopping cart icon
 
 function CartIcon() {
-  const cartItemCount = 3; // Replace this with your actual cart item count
+  // const cartItemCount = 3; 
 
   return (
-    <div className="fixed top-0 right-0 m-4 mt-8 pr-4">
+    <div className="relative top-0 right-0 ">
       <Link to="/cart">
         <div className="relative">
           <ShoppingCartIcon className="h-8 w-8 text-white hover:text-gray-300" />
-          {cartItemCount > 0 && (
-            <div className="absolute top-0 right-0 bg-red-500 text-white w-4 h-4 rounded-full flex items-center justify-center text-xs font-semibold">
-              {cartItemCount}
+          {/* {cartItemCount > 0 && ( */}
+            {/* <div className="absolute top-0 right-0 bg-red-500 text-white w-4 h-4 rounded-full flex items-center justify-center text-xs font-semibold"> */}
+              {/* {cartItemCount} */}
             </div>
-          )}
-        </div>
+          {/* )} */}
+        {/* </div> */}
       </Link>
       
     </div>
