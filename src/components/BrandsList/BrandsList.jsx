b86b2677ca0a3eb04ff9364798ea5ed8973f534d
letmechek <abdullahi.ali1017@gmail.com>
2023-10-13 20:18:32 -0400

    fixes image sizes on mobile layout

--- src/components/BrandsList/BrandsList.jsx
+++ src/components/BrandsList/BrandsList.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 
 export default function BrandsList({ cat }) {
-  return (
-    <Link to={`/model/${cat._id}`}>
-    <div
-      className="m-4 flex flex-col items-center justify-center pt-4 hover:scale-105">
-      <div className="bg-white pr-14 pl-14 p-4 m-2 shadow-xl rounded-lg flex justify-center items-center ">
-        <img src={cat.image} alt={cat.name} className="h-auto object-contain max-w-[100%]" />
-      </div>
+return (
+  <Link to={`/model/${cat._id}`}>
+  <div
+    className="m-4 flex flex-col items-center justify-center pt-4 hover:scale-105">
+    <div className="bg-white pr-7 pl-7 p-4 m-2 shadow-xl rounded-lg flex justify-center items-center ">
+      <img src={cat.image} alt={cat.name} className="w-full h-full object-contain" />
     </div>
-    </Link>
-  );
+  </div>
+  </Link>
+);
 }
-
