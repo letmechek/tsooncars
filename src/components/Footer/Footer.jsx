286822cf2e27da3c8b42052b2a034db37f6b46f2
letmechek <abdullahi.ali1017@gmail.com>
2023-11-04 18:20:11 -0400

    fixes footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -25,7 +25,7 @@ export default function FooterWithSitemap() {
   ];
   return (
 
-    <footer className="flex bg-stone-500 flex-col items-center text-center text-white">
+    <footer className="flex bg-white flex-col items-center text-center text-black ">
     <div className="container px-6 pt-6">
       <div className="mb-6 flex justify-center">
       <Link
@@ -60,23 +60,6 @@ export default function FooterWithSitemap() {
         </svg>
       </Link>
 
-      <Link
-        to="#!"
-        type="button"
-        class="m-1 h-9 w-9 rounded-full border-2 border-white uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0"
-        data-te-ripple-init
-        data-te-ripple-color="light">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="mx-auto h-full w-4"
-          fill="currentColor"
-          viewBox="0 0 24 24">
-          <path
-            d="M7 11v2.4h3.97c-.16 1.029-1.2 3.02-3.97 3.02-2.39 0-4.34-1.979-4.34-4.42 0-2.44 1.95-4.42 4.34-4.42 1.36 0 2.27.58 2.79 1.08l1.9-1.83c-1.22-1.14-2.8-1.83-4.69-1.83-3.87 0-7 3.13-7 7s3.13 7 7 7c4.04 0 6.721-2.84 6.721-6.84 0-.46-.051-.81-.111-1.16h-6.61zm0 0 17 2h-3v3h-2v-3h-3v-2h3v-3h2v3h3v2z"
-            fill-rule="evenodd"
-            clip-rule="evenodd" />
-        </svg>
-      </Link>
 
       <Link
         to="#!"
@@ -94,37 +77,6 @@ export default function FooterWithSitemap() {
         </svg>
       </Link>
 
-      <Link
-        to="#!"
-        type="button"
-        class="m-1 h-9 w-9 rounded-full border-2 border-white uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0"
-        data-te-ripple-init
-        data-te-ripple-color="light">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="mx-auto h-full w-4"
-          fill="currentColor"
-          viewBox="0 0 24 24">
-          <path
-            d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
-        </svg>
-      </Link>
-
-      <Link
-        to="#!"
-        type="button"
-        class="m-1 h-9 w-9 rounded-full border-2 border-white uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0"
-        data-te-ripple-init
-        data-te-ripple-color="light">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="mx-auto h-full w-4"
-          fill="currentColor"
-          viewBox="0 0 24 24">
-          <path
-            d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-        </svg>
-      </Link>
     </div>
 
     <div className="flex justify-center ">
@@ -132,7 +84,7 @@ export default function FooterWithSitemap() {
         <div
           class=" items-center gap-4 md:grid-cols-3">
           <div class="md:mb-6 md:ml-auto">
-            <p class="">
+            <p class=" ">
               <strong>Be the First to Know & Enjoy Exclusive Offers!</strong>
             </p>
           </div>
@@ -140,21 +92,21 @@ export default function FooterWithSitemap() {
           <div class="relative md:mb-6" data-te-input-wrapper-init>
           <input
                   type="text"
-                  className="peer block min-h-[auto] w-full rounded border border-black bg-transparent px-3 py-[0.32rem] leading-[1.6] text-neutral-200 outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                  className="peer block min-h-[auto] w-full rounded border border-black bg-transparent px-3 py-[0.32rem] leading-[1.6] text-black outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-black dark:placeholder:text-black [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                   id="exampleFormControlInput1"
                   placeholder="Email address"
                 />
             <label
               for="exampleFormControlInput1"
-              class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-200 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-neutral-200 peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
+              class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-black transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-neutral-200 peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
               >Email address
             </label>
           </div>
 
-          <div class="mb-6 md:mr-auto pt-2">
+          <div class="mb-6 md:mr-auto p-1.5">
             <button
               type="submit"
-              class="inline-block rounded border-2 border-neutral-50 px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
+              class="inline-block rounded border-2 border-neutral-50 px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-black transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
               data-te-ripple-init
               data-te-ripple-color="light">
               Submit
@@ -174,7 +126,7 @@ export default function FooterWithSitemap() {
               <ul className="mb-0 list-none">
                 {item.links.map((link, linkIndex) => (
                   <li key={linkIndex}>
-                    <Link to={link.to} className="text-white">
+                    <Link to={link.to} className="text-black">
                       {link.label}
                     </Link>
                   </li>
@@ -185,7 +137,7 @@ export default function FooterWithSitemap() {
         </div>
 
   </div>
-  <div class="mb-6 pr-8 pl-8 font-custom-font ">
+  <div class="mb-6 pr-8 pl-8 ">
       <p>
       Children are canvases, and Little Nest Apparel is a vision of this enduring philosophy. Our founders traversed the globe to gain youthful inspiration for what is now this much beloved children's brand. Luibelle today continues to mine the minds of fashion's finest artisans to create haute designs for young boys and girls. The line's artful attire blends luxury and whimsy, always conjuring an elegant sense of culture with the perfect dash of childhood innocence. Luibelle kids are sweet and dapper, bringing the avant-garde to the playground. From babies to toddlers and beyond, our exclusive fashions offer style and comfort to instill early confidence and help embrace self-expression. Luibelle apparel speaks to the posh parent who loves everything young, inspired and upscale. With children growing up so fast, Luibelle kindles their iconic style to last forever.
       </p>
@@ -195,7 +147,7 @@ export default function FooterWithSitemap() {
     class="w-full p-4 text-center"
    >
     © 2023 Copyright:
-    <Link class="text-white" to="https://tw-elements.com/">TW elements</Link>
+    <Link class="text-black" to="/"> Little Nest Apparel</Link>
   </div>
 </footer>
 
